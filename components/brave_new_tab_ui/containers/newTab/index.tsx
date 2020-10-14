2e9a3c8fc021bc46417a0e636a414d2866f944fc
Brian Clifton <brian@clifton.me>
2020-10-14 00:55:13 -0700

    Added `Brave.NTP.CustomizeUsageStatus`; Created new brave_p3a_utils

--- components/brave_new_tab_ui/containers/newTab/index.tsx
+++ components/brave_new_tab_ui/containers/newTab/index.tsx
@@ -459,6 +459,7 @@ class NewTabPage extends React.Component<Props, State> {
   }
 
   toggleSettings = () => {
+    this.props.actions.customizeClicked()
     if (this.state.showSettingsMenu) {
       this.setState({ focusMoreCards: false })
     }
